aa1f3eb7cceb387b7e58cf29a73a02c58b134141
Peter Zhang <zozohtnt@gmail.com>
2024-02-08 18:59:00 +0800

    add main-frame@mode=mobile

--- src/lib/shelf/main-frame/TiMainFrame.vue
+++ src/lib/shelf/main-frame/TiMainFrame.vue
@@ -1,104 +1,47 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { computed } from 'vue';
+import { MainFrameProps, getDemoContent } from './use-main-frame';
+
+const props = withDefaults(defineProps<MainFrameProps>(), {
+  mode: "Z"
+})
+
+const MFMode = computed(() => props.mode || "T")
+
+</script>
 <template>
-  <div class="ti-main-frame mode-Z">
-    <!--顶栏-->
-    <div class="frame-part as-sky">Sky</div>
-    <!--主区域-->
-    <div class="frame-part as-arena">
-      <div class="arena-con">
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
-        <p>非常多的内容</p>
+  <div class="ti-main-frame" :mode="MFMode">
+    <!-- 
+      =====Mobile Layout======  
+    -->
+    <template v-if="'mobile' == MFMode">
+      <!--顶栏-->
+      <div class="frame-part as-sky">Sky</div>
+      <!--主区域-->
+      <div class="frame-part as-arena">
+        <div class="arena-con" v-html="getDemoContent()"></div>
       </div>
-    </div>
-    <!--侧边栏-->
-    <div class="frame-part as-chute">Chute</div>
-    <!--底栏-->
-    <div class="frame-part as-foot">Foot</div>
+      <!--底栏-->
+      <div class="frame-part as-foot">Foot</div>
+    </template>
+    <!--
+      =====Desktop Layout====== 
+    -->
+    <template v-else>
+      <!--顶栏-->
+      <div class="frame-part as-sky">Sky</div>
+      <!--主区域-->
+      <div class="frame-part as-arena">
+        <div class="arena-con" v-html="getDemoContent()"></div>
+      </div>
+      <!--侧边栏-->
+      <div class="frame-part as-chute">Chute</div>
+      <!--底栏-->
+      <div class="frame-part as-foot">Foot</div>
+    </template>
   </div>
 </template>
 <style lang="scss" scoped>
-  @use '../../../assets/style/_all.scss' as *;
-  @import './ti-main-frame.scss';
+@use '../../../assets/style/_all.scss' as *;
+@import './ti-main-frame.scss';
 </style>
--- src/lib/shelf/main-frame/use-main-frame.ts
+++ src/lib/shelf/main-frame/use-main-frame.ts
@@ -1,1 +1,14 @@
-export const COM_TYPE = "TiMainFrame"
+export const COM_TYPE = 'TiMainFrame';
+
+export function getDemoContent() {
+  let html = [];
+  for (let i = 0; i < 100; i++) {
+    html.push(`<p>${i} - 非常多的内容</p>`);
+  }
+  return html.join('\n');
+}
+
+export type MainFrameMode = 'T' | 'C' | 'Z' | 'mobile';
+export type MainFrameProps = {
+  mode?: MainFrameMode;
+};
