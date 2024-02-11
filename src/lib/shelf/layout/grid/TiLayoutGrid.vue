f833e7b26c1b6b25b566c0336581a325efb254fb
Peter Zhang <zozohtnt@gmail.com>
2024-02-11 22:29:22 +0800

    ...

--- src/lib/shelf/layout/grid/TiLayoutGrid.vue
+++ src/lib/shelf/layout/grid/TiLayoutGrid.vue
@@ -1,109 +1,121 @@
 <script lang="ts" setup>
-import {
-  Ref,
-  computed,
-  inject,
-  onMounted,
-  onUnmounted,
-  reactive,
-  ref,
-  watch,
-} from 'vue';
-import {
-  BUS_KEY,
-  TiAppEvent,
-  TiBlock,
-  TiLayoutTabs,
-  useBusEmit,
-} from '../../../';
-import { TiLayoutGridInfo } from './ti-layout-grid-index';
-import { loadAllState, useKeepLayoutGrid } from './use-grid-keep';
-import {
-  COM_TYPE,
-  LayoutGridEvents,
-  LayoutGridProps,
-  LayoutGridState,
-  useLayoutGrid,
-} from './use-layout-grid';
-/*-------------------------------------------------------
+  import {
+    Ref,
+    computed,
+    inject,
+    onMounted,
+    onUnmounted,
+    reactive,
+    ref,
+    watch,
+  } from 'vue';
+  import {
+    BUS_KEY,
+    TiAppEvent,
+    TiBlock,
+    TiLayoutTabs,
+    useBusEmit,
+  } from '../../../';
+  import { TiLayoutGridInfo } from './ti-layout-grid-index';
+  import { loadAllState, useKeepLayoutGrid } from './use-grid-keep';
+  import {
+    COM_TYPE,
+    LayoutGridEvents,
+    LayoutGridProps,
+    LayoutGridState,
+    useLayoutGrid,
+  } from './use-layout-grid';
+  /*-------------------------------------------------------
 
                    Com Options
 
 -------------------------------------------------------*/
-defineOptions({
-  name: COM_TYPE,
-  inheritAttrs: true,
-});
-/*-------------------------------------------------------
+  defineOptions({
+    name: COM_TYPE,
+    inheritAttrs: true,
+  });
+  /*-------------------------------------------------------
 
                       State
 
 -------------------------------------------------------*/
-const $main: Ref<HTMLElement> = ref() as Ref<HTMLElement>;
-const state = reactive({
-  shown: {},
-  columns: [],
-  rows: [],
-}) as LayoutGridState;
-/*-------------------------------------------------------
+  const $main: Ref<HTMLElement> = ref() as Ref<HTMLElement>;
+  const state = reactive({
+    shown: {},
+    columns: [],
+    rows: [],
+  }) as LayoutGridState;
+  /*-------------------------------------------------------
 
                       Props
 
 -------------------------------------------------------*/
-const props = withDefaults(defineProps<LayoutGridProps>(), {});
+  const props = withDefaults(defineProps<LayoutGridProps>(), {});
 
-/*-------------------------------------------------------
+  /*-------------------------------------------------------
 
                 Bus & Notify & Emit
 
 -------------------------------------------------------*/
-let outer_bus = inject(BUS_KEY);
-let emit = defineEmits<{
-  (event: LayoutGridEvents, payload: TiAppEvent): void;
-}>();
-// 准备通知函数
-let notify = useBusEmit(TiLayoutGridInfo, props, emit, outer_bus);
-/*-------------------------------------------------------
+  let outer_bus = inject(BUS_KEY);
+  let emit = defineEmits<{
+    (event: LayoutGridEvents, payload: TiAppEvent): void;
+  }>();
+  // 准备通知函数
+  let notify = useBusEmit(TiLayoutGridInfo, props, emit, outer_bus);
+  /*-------------------------------------------------------
 
                   Use features
 
 -------------------------------------------------------*/
-let Grid = computed(() => useLayoutGrid(state, props, { notify }));
-let Keep = computed(() => useKeepLayoutGrid(props));
-/*-------------------------------------------------------
+  let Grid = computed(() => useLayoutGrid(state, props, { notify }));
+  let Keep = computed(() => useKeepLayoutGrid(props));
+  /*-------------------------------------------------------
 
                 Life Hooks
 
 -------------------------------------------------------*/
-watch(
-  () => [props.keepShown, props.keepSizes],
-  () => {
+  watch(
+    () => [props.keepShown, props.keepSizes],
+    () => {
+      loadAllState(props, state, Keep.value);
+    }
+  );
+  onMounted(() => {
+    Grid.value.bindResizing($main.value, state, onUnmounted, Keep);
     loadAllState(props, state, Keep.value);
-  },
-);
-onMounted(() => {
-  Grid.value.bindResizing($main.value, state, onUnmounted, Keep);
-  loadAllState(props, state, Keep.value);
-});
+  });
 </script>
 <template>
-  <div class="ti-layout-grid" :class="Grid.TopClass" :style="Grid.TopStyle"
+  <div
+    class="ti-layout-grid"
+    :class="Grid.TopClass"
+    :style="Grid.TopStyle"
     ref="$main">
     <!--
       Grid Blocks
     -->
-    <div v-for="it in Grid.Items" :key="it.uniqKey" class="grid-item"
-      :it-index="it.index" :it-ukey="it.uniqKey" :style="it.style">
+    <div
+      v-for="it in Grid.Items"
+      :key="it.uniqKey"
+      class="grid-item"
+      :it-index="it.index"
+      :it-ukey="it.uniqKey"
+      :style="it.style">
       <!-------- Block Container -------->
       <div class="grid-item-con" :class="it.className" :style="it.conStyle">
         <slot :item="it">
           <!-- 布局块-->
           <TiBlock v-if="'block' == it.type" v-bind="it.itemConfig" />
           <!-- 格子布局-->
-          <TiLayoutGrid v-else-if="'grid' == it.type" v-bind="it.itemConfig"
+          <TiLayoutGrid
+            v-else-if="'grid' == it.type"
+            v-bind="it.itemConfig"
             :schema="schema" />
           <!-- 标签布局-->
-          <TiLayoutTabs v-else-if="'tabs' == it.type" v-bind="it.itemConfig"
+          <TiLayoutTabs
+            v-else-if="'tabs' == it.type"
+            v-bind="it.itemConfig"
             :schema="schema" />
           <!-- 未知布局-->
           <div v-else>
@@ -113,40 +125,56 @@ onMounted(() => {
       </div>
       <!-------- 调整条 -------->
       <template v-if="it.adjustBars && it.adjustBars.length > 0">
-        <div v-for="bar in it.adjustBars" class="adjust-bar" :bar-mode="bar.mode"
-          :bar-adjust-index="bar.adjustIndex" :bar-position="bar.position"></div>
+        <div
+          v-for="bar in it.adjustBars"
+          class="adjust-bar"
+          :bar-mode="bar.mode"
+          :bar-adjust-index="bar.adjustIndex"
+          :bar-position="bar.position"></div>
       </template>
     </div>
     <!-- 
       Pop Panel
     -->
     <template v-for="pan in Grid.Panels" :key="pan.uniqKey">
-      <template v-if="!pan.hidden">
-        <div class="layout-panel" :class="pan.className" :panel-index="pan.index"
-          :panel-ukey="pan.uniqKey" :style="pan.style">
-          <div class="layout-panel-con" :style="pan.conStyle">
-            <slot name="panel" :panel="pan">
-              <!-- 布局块-->
-              <TiBlock v-if="'block' == pan.type" v-bind="pan.itemConfig" />
-              <!-- 格子布局-->
-              <TiLayoutGrid v-else-if="'grid' == pan.type" v-bind="pan.itemConfig"
-                :schema="schema" />
-              <!-- 标签布局-->
-              <TiLayoutTabs v-else-if="'tabs' == pan.type" v-bind="pan.itemConfig"
-                :schema="schema" />
-              <!-- 未知布局-->
-              <div v-else>
-                Unknown layout item type: <code>{{ pan.type }}</code>
-              </div>
-            </slot>
+      <Transition name="slide-left">
+        <template v-if="!pan.hidden">
+          <div
+            class="layout-panel"
+            :class="pan.className"
+            :panel-index="pan.index"
+            :panel-ukey="pan.uniqKey"
+            :style="pan.style">
+            <div class="layout-panel-con" :style="pan.conStyle">
+              <slot name="panel" :panel="pan">
+                <!-- 布局块-->
+                <TiBlock v-if="'block' == pan.type" v-bind="pan.itemConfig" />
+                <!-- 格子布局-->
+                <TiLayoutGrid
+                  v-else-if="'grid' == pan.type"
+                  v-bind="pan.itemConfig"
+                  :schema="schema" />
+                <!-- 标签布局-->
+                <TiLayoutTabs
+                  v-else-if="'tabs' == pan.type"
+                  v-bind="pan.itemConfig"
+                  :schema="schema" />
+                <!-- 未知布局-->
+                <div v-else>
+                  Unknown layout item type: <code>{{ pan.type }}</code>
+                </div>
+              </slot>
+            </div>
           </div>
-        </div>
-      </template>
+        </template>
+      </Transition>
     </template>
   </div>
 </template>
 <style lang="scss">
-@use '../../../../assets/style/_all.scss' as *;
-@import './ti-layout-grid';
-@import './../layout-panel';
+  @use '../../../../assets/style/_all.scss' as *;
+  @import './ti-layout-grid';
+  @import './../layout-panel';
+  @import './style/_grid-adjust-bar';
+  @import './style/_transition';
 </style>
--- src/lib/shelf/playground/TiPlayground.vue
+++ src/lib/shelf/playground/TiPlayground.vue
@@ -1,141 +1,163 @@
 <script setup lang="ts">
-import _ from 'lodash';
-import { Ref, computed, ref, watch } from 'vue';
-import { Vars } from '../../../core';
-import { tiCheckComponent } from '../../../lib';
-import TiLayoutGrid from '../layout/grid/TiLayoutGrid.vue';
-import PlayConf from './PlayConf.vue';
-import PlayLive from './PlayLive.vue';
-import PlayTabs from './PlayTabs.vue';
-import { useGridLayout } from './use-play-layout';
-import {
-  LiveBgMode,
-  PlayLayoutMode,
-  loadLiveBgMode,
-  loadPlayLayoutMode,
-  saveLiveBgMode,
-  savePlayLayoutMode,
-} from './use-play-mode';
-import {
-  ExampleState,
-  PlaygroundProps,
-  saveLocalSetting,
-  selectExample
-} from './use-playground';
-/**
- * 指明当前的控件对象要展示的示例配置信息
- */
-const _example = ref({} as ExampleState);
+  import _ from 'lodash';
+  import { Ref, computed, ref, watch } from 'vue';
+  import { Vars } from '../../../core';
+  import { tiCheckComponent } from '../../../lib';
+  import TiLayoutGrid from '../layout/grid/TiLayoutGrid.vue';
+  import PlayConf from './PlayConf.vue';
+  import PlayLive from './PlayLive.vue';
+  import PlayTabs from './PlayTabs.vue';
+  import { useGridLayout } from './use-play-layout';
+  import {
+    LiveBgMode,
+    PlayLayoutMode,
+    loadLiveBgMode,
+    loadPlayLayoutMode,
+    saveLiveBgMode,
+    savePlayLayoutMode,
+  } from './use-play-mode';
+  import {
+    ExampleState,
+    PlaygroundProps,
+    saveLocalSetting,
+    selectExample,
+  } from './use-playground';
+  /**
+   * 指明当前的控件对象要展示的示例配置信息
+   */
+  const _example = ref({} as ExampleState);
 
-/*
- * 指明布局显示模式
- */
-const _play_layout_mode: Ref<PlayLayoutMode> = ref(loadPlayLayoutMode());
-/**
- * 指明控件演示区的布局模式
- */
-const _live_bg_mode: Ref<LiveBgMode> = ref(loadLiveBgMode());
-/**
- * 如果 play_mode == 'F' 那么需要指定是否隐藏显示 conf
- */
-const _layout_shown: Ref<Record<string, boolean>> = ref({
-  conf: true
-})
+  /*
+   * 指明布局显示模式
+   */
+  const _play_layout_mode: Ref<PlayLayoutMode> = ref(loadPlayLayoutMode());
+  /**
+   * 指明控件演示区的布局模式
+   */
+  const _live_bg_mode: Ref<LiveBgMode> = ref(loadLiveBgMode());
+  /**
+   * 如果 play_mode == 'F' 那么需要指定是否隐藏显示 conf
+   */
+  const _layout_shown: Ref<Record<string, boolean>> = ref({});
 
-/**
- * 本控件要接入的属性
- */
-const props = withDefaults(defineProps<PlaygroundProps>(), {
-  comType: 'TiUnkown',
-  example: '',
-  exampleAsRouterLink: false,
-});
+  /**
+   * 本控件要接入的属性
+   */
+  const props = withDefaults(defineProps<PlaygroundProps>(), {
+    comType: 'TiUnkown',
+    example: '',
+    exampleAsRouterLink: false,
+  });
 
-/**
- * 指明当前的控件对象
- */
-const PlayCom = computed(() => {
-  return tiCheckComponent(props.comType);
-});
+  /**
+   * 指明当前的控件对象
+   */
+  const PlayCom = computed(() => {
+    return tiCheckComponent(props.comType);
+  });
 
-const ExampleChanged = computed(() => {
-  let dftComConf = PlayCom.value.checkProps(_example.value.name);
-  return !_.isEqual(_example.value.comConf, dftComConf)
-})
+  const ExampleChanged = computed(() => {
+    let dftComConf = PlayCom.value.checkProps(_example.value.name);
+    return !_.isEqual(_example.value.comConf, dftComConf);
+  });
 
-const Grid = computed(() => useGridLayout(_play_layout_mode.value));
+  const Grid = computed(() => useGridLayout(_play_layout_mode.value));
 
-function OnSelectExample(name: string) {
-  selectExample(PlayCom.value, _example.value, name);
-}
-
-function OnConfReset() {
-  let dftComConf = PlayCom.value.checkProps(_example.value.name);
-  _example.value.comConf = dftComConf
-}
-
-function OnConfChange(confData: Vars) {
-  _example.value.comConf = confData
-  saveLocalSetting(PlayCom.value, _example.value)
-}
-
-watch(
-  () => props.example,
-  function (exampleName) {
-    let com = PlayCom.value;
-    selectExample(com, _example.value, exampleName);
-  },
-  {
-    immediate: true,
+  function OnSelectExample(name: string) {
+    selectExample(PlayCom.value, _example.value, name);
   }
-);
 
-watch(() => [_live_bg_mode.value, _play_layout_mode.value], function (payload) {
-  let [live_bg, play_layout] = payload
-  saveLiveBgMode(live_bg as LiveBgMode);
-  savePlayLayoutMode(play_layout as PlayLayoutMode);
-})
+  function OnConfReset() {
+    let dftComConf = PlayCom.value.checkProps(_example.value.name);
+    _example.value.comConf = dftComConf;
+  }
+
+  function OnConfChange(confData: Vars) {
+    _example.value.comConf = confData;
+    saveLocalSetting(PlayCom.value, _example.value);
+  }
+
+  function OnToggleConfShown() {
+    if (_layout_shown.value.conf) {
+      _layout_shown.value.conf = false;
+    } else {
+      _layout_shown.value.conf = true;
+    }
+  }
+
+  watch(
+    () => props.example,
+    function (exampleName) {
+      let com = PlayCom.value;
+      selectExample(com, _example.value, exampleName);
+    },
+    {
+      immediate: true,
+    }
+  );
+
+  watch(
+    () => [_live_bg_mode.value, _play_layout_mode.value],
+    function (payload) {
+      let [live_bg, play_layout] = payload;
+      saveLiveBgMode(live_bg as LiveBgMode);
+      savePlayLayoutMode(play_layout as PlayLayoutMode);
+    }
+  );
 </script>
 
 <template>
   <div class="ti-playground" :mode="_play_layout_mode">
-    <TiLayoutGrid v-bind="Grid" :style="{
-      flex: '1 1 auto',
-      padding: '10px',
-    }" :shown="_layout_shown">
+    <TiLayoutGrid
+      v-bind="Grid"
+      :style="{
+        flex: '1 1 auto',
+        padding: '10px',
+      }"
+      :shown="_layout_shown">
       <template v-slot="{ item }">
         <!--
             Tabs
         -->
         <template v-if="'tabs' == item.name">
-          <PlayTabs :com="PlayCom" :current-example="_example.name"
-            :play-layout-mode="_play_layout_mode" :live-bg-mode="_live_bg_mode"
+          <PlayTabs
+            :com="PlayCom"
+            :current-example="_example.name"
+            :play-layout-mode="_play_layout_mode"
+            :live-bg-mode="_live_bg_mode"
             :example-as-router-link="exampleAsRouterLink"
             @change="OnSelectExample"
-            @mode:layout="_play_layout_mode = ($event as PlayLayoutMode)"
-            @mode:live_bg="_live_bg_mode = ($event as LiveBgMode)" />
+            @mode:layout="_play_layout_mode = $event as PlayLayoutMode"
+            @mode:live_bg="_live_bg_mode = $event as LiveBgMode"
+            @toggle:conf="OnToggleConfShown" />
         </template>
         <!--
           Live
         -->
         <template v-else-if="'live' == item.name">
-          <PlayLive :play-com="PlayCom" :play-conf="_example.comConf"
+          <PlayLive
+            :play-com="PlayCom"
+            :play-conf="_example.comConf"
             :mode="_live_bg_mode" />
         </template>
         <!--
           Conf
         -->
         <template v-else>
-          <PlayConf :config-data="_example.comConf"
-            :config-changed="ExampleChanged" @change="OnConfChange"
+          <PlayConf
+            :config-data="_example.comConf"
+            :config-changed="ExampleChanged"
+            @change="OnConfChange"
             @reset="OnConfReset" />
         </template>
       </template>
 
       <template name="panel" v-slot:panel="{ panel }">
         <template v-if="'conf' == panel.name">
-          <PlayConf :config-data="_example.comConf"
-            :config-changed="ExampleChanged" @change="OnConfChange"
+          <PlayConf
+            :config-data="_example.comConf"
+            :config-changed="ExampleChanged"
+            @change="OnConfChange"
             @reset="OnConfReset" />
         </template>
       </template>
@@ -144,6 +166,6 @@ watch(() => [_live_bg_mode.value, _play_layout_mode.value], function (payload) {
 </template>
 
 <style lang="scss" scoped>
-@use '../../../assets/style/_all.scss' as *;
-@import './playground.scss';
+  @use '../../../assets/style/_all.scss' as *;
+  @import './playground.scss';
 </style>
