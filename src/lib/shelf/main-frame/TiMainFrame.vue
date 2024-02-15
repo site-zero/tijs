df3f2bae87473e74007dee02872af76902d5b3b7
Peter Zhang <zozohtnt@gmail.com>
2024-02-15 18:12:07 +0800

    ...

--- src/lib/shelf/main-frame/TiMainFrame.vue
+++ src/lib/shelf/main-frame/TiMainFrame.vue
@@ -1,25 +1,30 @@
 <script lang="ts" setup>
-import { computed } from 'vue';
-import { MainFrameProps, getDemoContent } from './use-main-frame';
+  import { computed } from 'vue';
+  import { MainFrameProps, getDemoContent } from './use-main-frame';
 
-const props = withDefaults(defineProps<MainFrameProps>(), {
-  mode: "Z"
-})
+  const props = withDefaults(defineProps<MainFrameProps>(), {
+    mode: 'Z',
+  });
 
-const MFMode = computed(() => props.mode || "T")
-
+  const MFMode = computed(() => props.mode || 'T');
 </script>
 <template>
-  <div class="ti-main-frame" :mode="MFMode">
+  <div
+    class="ti-main-frame"
+    :mode="MFMode">
     <!-- 
       =====Mobile Layout======  
     -->
     <template v-if="'mobile' == MFMode">
       <!--顶栏-->
-      <div class="frame-part as-sky">Sky</div>
+      <div class="frame-part as-sky">
+        <slot name="sky"><span>{Slot Sky}</span></slot>
+      </div>
       <!--主区域-->
       <div class="frame-part as-arena">
-        <div class="arena-con" v-html="getDemoContent()"></div>
+        <div
+          class="arena-con"
+          v-html="getDemoContent()"></div>
       </div>
       <!--底栏-->
       <div class="frame-part as-foot">Foot</div>
@@ -29,10 +34,14 @@ const MFMode = computed(() => props.mode || "T")
     -->
     <template v-else>
       <!--顶栏-->
-      <div class="frame-part as-sky">Sky</div>
+      <div class="frame-part as-sky">
+        <slot name="sky"><span>{Slot Sky}</span></slot>
+      </div>
       <!--主区域-->
       <div class="frame-part as-arena">
-        <div class="arena-con" v-html="getDemoContent()"></div>
+        <div
+          class="arena-con"
+          v-html="getDemoContent()"></div>
       </div>
       <!--侧边栏-->
       <div class="frame-part as-chute">Chute</div>
@@ -42,6 +51,6 @@ const MFMode = computed(() => props.mode || "T")
   </div>
 </template>
 <style lang="scss" scoped>
-@use '../../../assets/style/_all.scss' as *;
-@import './ti-main-frame.scss';
+  @use '../../../assets/style/_all.scss' as *;
+  @import './ti-main-frame.scss';
 </style>
