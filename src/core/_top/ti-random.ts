f35ead176a5d8772214b5c0a05dfea86e95a2821
Peter Zhang <zozohtnt@gmail.com>
2024-10-02 21:01:54 +0800

    ...

--- src/core/_top/ti-random.ts
+++ src/core/_top/ti-random.ts
@@ -1,23 +1,35 @@
 import _ from 'lodash';
 
-const CHARS =
-  '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'.split('');
+const CHAR_62 =
+  '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghijklmnopqrstuvwxyz'.split('');
+const CHAR_36 = '0123456789abcdefghijklmnopqrstuvwxtz'.split('');
 /***
  * Generator `N` length random string
  */
-export function getRandomStr(n = 4, dict = CHARS) {
+export function getRandomStr(n = 4) {
   let s = '';
   for (let i = 0; i < n; i++) {
-    let index = _.random(0, CHARS.length - 1);
-    s += dict[index];
+    let index = _.random(0, CHAR_62.length - 1);
+    s += CHAR_62[index];
   }
   return s;
 }
-export function getRandomObj(dict = CHARS) {
-  let index = _.random(0, CHARS.length - 1);
+
+export function getRandomS36(n = 4) {
+  let s = '';
+  for (let i = 0; i < n; i++) {
+    let index = _.random(0, CHAR_36.length - 1);
+    s += CHAR_36[index];
+  }
+  return s;
+}
+
+export function getRandomObj<T>(dict: T[]) {
+  let index = _.random(0, dict.length - 1);
   return dict[index];
 }
-export function getRandomList(input = [], n = input.length) {
+
+export function getRandomList<T>(input: T[] = [], n = input.length) {
   let last = Math.min(n, input.length) - 1;
   for (; last > 0; last--) {
     let index = _.random(0, last);
