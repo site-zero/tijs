aaa2f60fe4b8ff0bd46296d499dbbb006c719cc5
Peter Zhang <zozohtnt@gmail.com>
2024-11-25 15:05:57 +0800

    ...

--- src/lib/input/currency/TiInputCurrency.vue
+++ src/lib/input/currency/TiInputCurrency.vue
@@ -1,18 +1,54 @@
 <script lang="ts" setup>
+  import _ from 'lodash';
   import { computed } from 'vue';
-  import { TiInput } from '../../';
+  import { InputNumUnitProps, TiInputNumUnit } from '../../';
+  import { Vars } from '../../../_type';
+  import { InputCurrencyProps } from './ti-input-currency-types';
   //-----------------------------------------------------
-  defineOptions({
-    inheritAttrs: false,
+  const emit = defineEmits<{
+    (event: 'change', val: Vars): void;
+  }>();
+  //-----------------------------------------------------
+  const props = withDefaults(defineProps<InputCurrencyProps>(), {
+    precision: 100,
+    decimalPlaces: 2,
+    defaultCurrency: 'CNY',
+    numAsStr: true,
+    currencyWidth: '4em',
+    autoSelect: true,
   });
   //-----------------------------------------------------
-  let emit = defineEmits<{
-    (event: 'change', payload: string): void;
-  }>();
+  const InputNumUnitConfig = computed(() => {
+    return {
+      autoSelect: props.autoSelect,
+      readonly: props.readonly,
+      defaultNumber: 0,
+      defaultUnit: props.defaultCurrency,
+      precision: props.precision,
+      decimalPlaces: props.decimalPlaces,
+      numAsStr: props.numAsStr,
+      unitSelectOnly: false,
+      unitWidth: props.currencyWidth,
+      valueInputAlign: 'right',
+      getNumber: _.first(props.valueKeys),
+      getUnit: _.last(props.valueKeys),
+      units: {
+        readonly: props.readonly,
+        ...props.currencies,
+        valueCase: 'upperAll',
+        tipFormat: 'VT',
+        tipUseHint: true,
+        useRawValue: true,
+        tipListMinWidth: props.tipListMinWidth,
+      },
+    } as InputNumUnitProps;
+  });
+  //-----------------------------------------------------
 </script>
 <template>
-  <div>Unknown Components</div>
+  <TiInputNumUnit
+    v-bind="InputNumUnitConfig"
+    :value="props.value"
+    :readonly="props.readonly"
+    @change="emit('change', $event)" />
 </template>
-<style lang="scss" scoped>
-  @use './ti-input-currency.scss';
-</style>
--- src/lib/input/currency/ti-input-currency-types.ts
+++ src/lib/input/currency/ti-input-currency-types.ts
@@ -1,7 +1,21 @@
-import { ReadonlyProps } from '../../';
-import { CommonProps } from '../../../_type';
+import { OptionsProps, ReadonlyProps } from '../../';
+import { CommonProps, Vars } from '../../../_type';
 
-export type InputCurrencyProps = CommonProps & ReadonlyProps & {
-  value?: number|string
-};
+export type InputCurrencyProps = CommonProps &
+  ReadonlyProps & {
+    value?: Vars;
+    valueKeys?: [string, string];
 
+    currencies: OptionsProps;
+    precision?: number | undefined;
+    decimalPlaces?: number;
+
+    autoSelect?: boolean;
+
+    numAsStr?: boolean;
+    defaultCurrency?: string;
+
+    currencyWidth?: number | string;
+
+    tipListMinWidth?: number | string;
+  };
