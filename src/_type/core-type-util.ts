1596c65a5c6d9307a1e3f34fa1293310182f18fb
Peter Zhang <zozohtnt@gmail.com>
2024-12-05 21:25:45 +0800

    ...

--- src/_type/core-type-util.ts
+++ src/_type/core-type-util.ts
@@ -1,2 +1,22 @@
+import _ from 'lodash';
+
 export type SelectValueArmItem<T, M> = T | [T, M];
 export type SelectValueArm<T, M> = T | Array<SelectValueArmItem<T, M>>;
+
+export type Gender = 'UNKNOWN' | 'MALE' | 'FEMALE';
+
+export function toGender(str?: any): Gender {
+  if (_.isNil(str)) {
+    return 'UNKNOWN';
+  }
+  let lower = str.toLowerCase().trim();
+  if (/^(m(ale)?)$/.test(lower)) {
+    return 'MALE';
+  }
+
+  if (/^(f(emale)?)$/.test(lower)) {
+    return 'FEMALE';
+  }
+
+  return 'UNKNOWN';
+}
