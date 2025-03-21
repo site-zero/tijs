ff0b79d9a0978f65002a98a97d1f2feb38fb3b33
Peter Zhang <zozohtnt@gmail.com>
2025-03-22 02:57:49 +0800

    ...

--- src/_type/lib-type-row-indent.ts
+++ src/_type/lib-type-row-indent.ts
@@ -1,14 +1,14 @@
-import { IconInput, TableRowID } from "index";
+import { TableRowID } from './core-types';
 
-export type RowIndentStatus = 'open' | 'closed'
+export type RowIndentStatus = 'open' | 'closed';
 
 /**
  * 通过这个类型， `List` 与 `Table` 就很容易扩展为支持树型数据的显示。
- * 
+ *
  * 对于一个 `Row` 来说，通常有两个图标:
- * 
+ *
  * ```
- *         | indicator 指示图标, 
+ *         | indicator 指示图标,
  *  +----- | 肯定有且有两种状态
  *  V
  * [+] [C] node title
@@ -17,30 +17,29 @@ export type RowIndentStatus = 'open' | 'closed'
  *              | 可能有两种状态或者固定一种状态，
  *              | 也可能没有
  * ```
- * 
+ *
  * 根据下面的属性，可以知道每个行的各个状态图标
  */
 export type RowIndentProps = {
-    /**
-     * 指明缩进级数。0 表示不缩进
-     */
-    rowIndents?: Map<TableRowID, number>;
+  /**
+   * 指明缩进级数。0 表示不缩进
+   */
+  rowIndents?: Map<TableRowID, number>;
 
-    /**
-     * 指明各行状态，默认 'closed'
-     */
-    rowStauts?: Map<TableRowID, RowIndentStatus>;
+  /**
+   * 指明各行状态，默认 'closed'
+   */
+  rowStauts?: Map<TableRowID, RowIndentStatus>;
 
-    /**
-     * 行记号图标，只有声明了 rowStatus 才会生效
-     * 
-     * 默认为 `{open:'<i class="zmdi zmdi-minus"></i>', closed:'<i class="zmdi zmdi-plus"></i>S'}
-     */
-    rowIndicators?: Record<RowIndentStatus, string>;
+  /**
+   * 行记号图标，只有声明了 rowStatus 才会生效
+   *
+   * 默认为 `{open:'<i class="zmdi zmdi-minus"></i>', closed:'<i class="zmdi zmdi-plus"></i>S'}
+   */
+  rowIndicators?: Record<RowIndentStatus, string>;
 
-    /**
-     * 行状态图标
-     */
-    rowMarkers?: string | Record<RowIndentStatus, string>;
-
-}
+  /**
+   * 行状态图标
+   */
+  rowMarkers?: string | Record<RowIndentStatus, string>;
+};
