ae22543f2ca595bd00b4036f884f11f29fbb2e68
Peter Zhang <zozohtnt@gmail.com>
2025-05-18 02:32:46 +0800

    refactor(lbs-map): 重构地图初始化逻辑并优化事件处理
    重构了 `initMap` 函数，将事件处理逻辑从 `this` 上下文转移到 `api` 对象，以提高代码的可维护性和可读性。同时，优化了 `redrawMap` 函数，确保地图数据渲染时使用正确的 `props` 配置。此次改动不影响现有功能，但使代码结构更加清晰。

--- src/lib/view/lbs-map/use-lbs-map-view.ts
+++ src/lib/view/lbs-map/use-lbs-map-view.ts
@@ -1,158 +1,192 @@
+import L, { LatLngTuple } from "leaflet";
 import _ from "lodash";
-import { getLBSMapStdTileLayer, isLatLngObj, isLatLngTuple, isLBSMapStdTileType, LatLngObj, LBSMapData, LBSMapDrawContext, LbsMapProps, LBSMapTileLayer, LBSMapValueCoords } from "./ti-lbs-map-types";
-import { getBounds, getLatlngObjBounds, getLatlngTupleBounds, latlngObjToTuple, latlngTupleToObj, translateCoordsForLatlngObj } from "./gis";
-import L, { LatLngTuple } from 'leaflet'
+import {
+  getLatlngObjBounds,
+  getLatlngTupleBounds,
+  latlngObjToTuple,
+  latlngTupleToObj,
+  translateCoordsForLatlngObj,
+} from "./gis";
+import {
+  isLatLngObj,
+  isLatLngTuple,
+  LatLngObj,
+  LBSMapData,
+  LBSMapDrawContext,
+  LbsMapProps,
+} from "./ti-lbs-map-types";
+import { LbsMapApi } from "./use-lbs-map";
 import { useLLTileLayer } from "./use-lbs-map-tiles";
 
-export function initMap(getMainElement: () => HTMLElement | null, props: LbsMapProps, _dc: LBSMapDrawContext, mapData: LBSMapData | null) {
-    let $main = getMainElement();
-    if (!$main) {
-        return;
-    }
-    // 注销已经存在的 Map
-    if (_dc.$live) {
-        _dc.$live.off()
-        _dc.$live.remove();
-        _dc.$live = undefined;
-    }
-    if (_dc.$map) {
-        _dc.$map.off();
-        _dc.$map.remove();
-        _dc.$map = undefined;
-    }
-    // Create Map
-    _dc.$map = L.map($main, {
-        ...props.mapOptions,
-        attributionControl: false,
-        minZoom: props.minZoom,
-        maxZoom: props.maxZoom
-    });
+export function initMap(
+  getMainElement: () => HTMLElement | null,
+  props: LbsMapProps,
+  _dc: LBSMapDrawContext,
+  api: LbsMapApi,
+  mapData: LBSMapData | null
+) {
+  let $main = getMainElement();
+  if (!$main) {
+    return;
+  }
+  // 注销已经存在的 Map
+  if (_dc.$live) {
+    _dc.$live.off();
+    _dc.$live.remove();
+    _dc.$live = undefined;
+  }
+  if (_dc.$map) {
+    _dc.$map.off();
+    _dc.$map.remove();
+    _dc.$map = undefined;
+  }
+  // Create Map
+  _dc.$map = L.map($main, {
+    ...props.mapOptions,
+    attributionControl: false,
+    minZoom: props.minZoom,
+    maxZoom: props.maxZoom,
+  });
 
-    L.control.scale({
-        metric: true,
-        imperial: false,
-        updateWhenIdle: true
-    }).addTo(_dc.$map);
+  L.control
+    .scale({
+      metric: true,
+      imperial: false,
+      updateWhenIdle: true,
+    })
+    .addTo(_dc.$map);
 
-    // Create the main bg-layer
-    useLLTileLayer(props);
+  // Create the main bg-layer
+  useLLTileLayer(props);
 
-    // Events
-    _dc.$map.on("move", (evt) => { this.OnMapMove(evt) })
-    _dc.$map.on("click", (evt) => { this.OnMapPointerClick(evt) })
-    _dc.$map.on("mousemove", (evt) => { this.OnMapPointerMove(evt) })
+  // Events
+  _dc.$map.on("move", (evt) => {
+    api.OnMapMove(evt);
+  });
+  _dc.$map.on("click", (evt) => {
+    api.OnMapPointerClick(evt);
+  });
+  _dc.$map.on("mousemove", (evt) => {
+    api.OnMapPointerMove(evt);
+  });
 
-    // Prepare live layer for the presentation of value data 
-    _dc.$live = L.layerGroup().addTo(_dc.$map)
+  // Prepare live layer for the presentation of value data
+  _dc.$live = L.layerGroup().addTo(_dc.$map);
 
+  // Init map view
+  initMapView(props, _dc, mapData);
 
-    // Init map view
-    initMapView(props, _dc, mapData);
-
-    // Then Render the data
-    redrawMap(props, _dc, mapData);
+  // Then Render the data
+  redrawMap(props, _dc, mapData);
 }
 
-export function initMapView(props: LbsMapProps, _dc: LBSMapDrawContext, mapData: LBSMapData | null) {
-    //console.log("initMapView")
-    // Get current zoom, keep the last user zoom state
-    let zoom = _dc.geo.zoom || props.zoom
+export function initMapView(
+  props: LbsMapProps,
+  _dc: LBSMapDrawContext,
+  mapData: LBSMapData | null
+) {
+  //console.log("initMapView")
+  // Get current zoom, keep the last user zoom state
+  let zoom = _dc.geo.zoom || props.zoom;
 
-    let fromCoords = props.valueCoords ?? 'WGS84'
-    let toCoords = _dc.baseTileCoords;
+  let fromCoords = props.valueCoords ?? "WGS84";
+  let toCoords = _dc.baseTileCoords;
 
-    // Default view
-    if (mapData && _dc.$map) {
-        // 采用北京作为默认的位置
-        let lal: LatLngObj = {
-            lat: 39.97773512677837,
-            lng: 116.3385673945887
-        }
-        if (props.defaultLocation) {
-            if (isLatLngObj(props.defaultLocation)) {
-                lal = props.defaultLocation
-            } else if (isLatLngTuple(props.defaultLocation)) {
-                lal = latlngTupleToObj(props.defaultLocation);
-            } else {
-                throw 'Invalid defaultLocation: ' + props.defaultLocation
-            }
-        }
-        let dftCenter = translateCoordsForLatlngObj(fromCoords, toCoords, lal);
-        _dc.$map.setView(dftCenter, zoom)
-        return
+  // Default view
+  if (mapData && _dc.$map) {
+    // 采用北京作为默认的位置
+    let lal: LatLngObj = {
+      lat: 39.97773512677837,
+      lng: 116.3385673945887,
+    };
+    if (props.defaultLocation) {
+      if (isLatLngObj(props.defaultLocation)) {
+        lal = props.defaultLocation;
+      } else if (isLatLngTuple(props.defaultLocation)) {
+        lal = latlngTupleToObj(props.defaultLocation);
+      } else {
+        throw "Invalid defaultLocation: " + props.defaultLocation;
+      }
     }
+    let dftCenter = translateCoordsForLatlngObj(fromCoords, toCoords, lal);
+    _dc.$map.setView(dftCenter, zoom);
+    return;
+  }
 
-    // Auto fit the data
-    //..................................
-    if ("obj" == props.valueType) {
-        let lal = mapData as LatLngObj;
-        _dc.$map!.setView(lal, zoom)
+  // Auto fit the data
+  //..................................
+  if ("obj" == props.valueType) {
+    let lal = mapData as LatLngObj;
+    _dc.$map!.setView(lal, zoom);
+  }
+  //..................................
+  else if ("obj-list" == props.valueType) {
+    let list = mapData as LatLngObj[];
+    if (list.length > 1) {
+      let gr = getLatlngObjBounds(list);
+      let SW = latlngObjToTuple(gr.SW);
+      let NE = latlngObjToTuple(gr.NE);
+      _dc.$map!.fitBounds([SW, NE]);
+    } else if (list.length == 1) {
+      let latlng = list[0];
+      _dc.$map!.setView(latlng, zoom);
     }
-    //..................................
-    else if ("obj-list" == props.valueType) {
-        let list = mapData as LatLngObj[];
-        if (list.length > 1) {
-            let gr = getLatlngObjBounds(list)
-            let SW = latlngObjToTuple(gr.SW)
-            let NE = latlngObjToTuple(gr.NE)
-            _dc.$map!.fitBounds([SW, NE])
-
-        } else if (list.length == 1) {
-            let latlng = list[0]
-            _dc.$map!.setView(latlng, zoom)
-        }
+  }
+  //..................................
+  else if ("tuple" == props.valueType) {
+    let lal = latlngTupleToObj(mapData as LatLngTuple);
+    _dc.$map!.setView(lal, zoom);
+  }
+  //..................................
+  else if ("tuple-list" == props.valueType) {
+    let list = mapData as LatLngTuple[];
+    if (list.length > 1) {
+      let gr = getLatlngTupleBounds(list);
+      let SW = latlngObjToTuple(gr.SW);
+      let NE = latlngObjToTuple(gr.NE);
+      _dc.$map!.fitBounds([SW, NE]);
+    } else if (list.length == 1) {
+      let latlng = list[0];
+      _dc.$map!.setView(latlng, zoom);
     }
-    //..................................
-    else if ("tuple" == props.valueType) {
-        let lal = latlngTupleToObj(mapData as LatLngTuple)
-        _dc.$map!.setView(lal, zoom)
-    }
-    //..................................
-    else if ("tuple-list" == props.valueType) {
-        let list = mapData as LatLngTuple[];
-        if (list.length > 1) {
-            let gr = getLatlngTupleBounds(list)
-            let SW = latlngObjToTuple(gr.SW)
-            let NE = latlngObjToTuple(gr.NE)
-            _dc.$map!.fitBounds([SW, NE])
-        } else if (list.length == 1) {
-            let latlng = list[0]
-            _dc.$map!.setView(latlng, zoom)
-        }
-    }
-    //..................................
-    else if ("geojson" == props.valueType) {
-        throw "Not implement geojson get center"
-    }
-    //..................................
+  }
+  //..................................
+  else if ("geojson" == props.valueType) {
+    throw "Not implement geojson get center";
+  }
+  //..................................
 }
 
-function redrawMap(props: LbsMapProps, _dc: LBSMapDrawContext, mapData: LBSMapData | null) {
-    let { $map, $live } = _dc;
-    if (!$map || !$live) {
-        return;
+function redrawMap(
+  props: LbsMapProps,
+  _dc: LBSMapDrawContext,
+  mapData: LBSMapData | null
+) {
+  let { $map, $live } = _dc;
+  if (!$map || !$live) {
+    return;
+  }
+
+  $map.invalidateSize();
+  // Prepare the function name
+
+  // Clear live layer
+  //$live.clearLayers()
+
+  // Draw data
+  if (mapData) {
+    let func = this[this.RedrawFuncName];
+    if (_.isFunction(func)) {
+      func(mapData, {
+        autoFitBounds: props.autoFitBounds,
+        showMarker: props.showMarker,
+        markerIcon: props.markerIcon,
+        markerIconOptions: props.markerIconOptions,
+        markerPopup: props.markerPopup,
+        markerPopupOptions: props.markerPopupOptions,
+      });
+    } else {
+      throw `Invalid RedrawFuncName="${this.RedrawFuncName}"`;
     }
-
-    $map.invalidateSize()
-    // Prepare the function name
-
-    // Clear live layer
-    //$live.clearLayers()
-
-    // Draw data
-    if (mapData) {
-        let func = this[this.RedrawFuncName]
-        if (_.isFunction(func)) {
-            func(this.MapData, {
-                autoFitBounds: this.autoFitBounds,
-                showMarker: this.showMarker,
-                markerIcon: this.markerIcon,
-                markerIconOptions: this.markerIconOptions,
-                markerPopup: this.markerPopup,
-                markerPopupOptions: this.markerPopupOptions
-            })
-        } else {
-            throw `Invalid RedrawFuncName="${this.RedrawFuncName}"`
-        }
-    }
+  }
 }
--- src/lib/view/lbs-map/use-lbs-map.ts
+++ src/lib/view/lbs-map/use-lbs-map.ts
@@ -3,177 +3,234 @@ import _ from "lodash";
 import { IconInput, LogicType } from "../../../_type";
 import { Icons, Num } from "../../../core";
 import { translateCoordsForLatlngObj } from "./gis/use-lbs-coords";
-import { getLBSMapStdTileLayer, isLBSMapStdTileType, LatLngObj, LBSMapData, LBSMapDrawContext, LBSMapEditMarkerIconOptions, LbsMapEmitter, LbsMapProps, LBSMapTileLayer, LBSMapValueCoords } from "./ti-lbs-map-types";
+import {
+  getLBSMapStdTileLayer,
+  isLBSMapStdTileType,
+  LatLngObj,
+  LBSMapData,
+  LBSMapDrawContext,
+  LBSMapEditMarkerIconOptions,
+  LbsMapEmitter,
+  LbsMapProps,
+  LBSMapTileLayer,
+  LBSMapValueCoords,
+} from "./ti-lbs-map-types";
 
-export type LbsMapApi = ReturnType<typeof useLbsMap>
+export type LbsMapApi = ReturnType<typeof useLbsMap>;
 
 export type LbsMapIconOptions = {
-    // 整体大小，默认 32
-    size?: number,
-    className?: any,
-    color?: LogicType,
-    // 图标大小: [24, 41]
-    iconSize?: [number, number],
-    // 锚点位置: [12, 41]
-    iconAnchor?: [number, number],
-    // 显示阴影
-    shadow?: boolean | string,
-    // 阴影大小: [41, 41]
-    shadowSize?: [number, number],
-    // 阴影锚点: [12, 41]
-    shadowAnchor?: [number, number],
+  // 整体大小，默认 32
+  size?: number;
+  className?: any;
+  color?: LogicType;
+  // 图标大小: [24, 41]
+  iconSize?: [number, number];
+  // 锚点位置: [12, 41]
+  iconAnchor?: [number, number];
+  // 显示阴影
+  shadow?: boolean | string;
+  // 阴影大小: [41, 41]
+  shadowSize?: [number, number];
+  // 阴影锚点: [12, 41]
+  shadowAnchor?: [number, number];
 };
 
-export function useLbsMap(props: LbsMapProps, _dc: LBSMapDrawContext, emit: LbsMapEmitter) {
-    // 初始化设置基础瓦片层坐标系
-    _dc.baseTileCoords = getBaseTileCoords(props);
-    //--------------------------------------------
-    // 帮助方法
-    //--------------------------------------------
-    function Icon(
-        urlOrIcon: IconInput,
-        options: LBSMapEditMarkerIconOptions = {}) {
-        if (!urlOrIcon)
-            return new L.Icon.Default()
+export function useLbsMap(
+  props: LbsMapProps,
+  _dc: LBSMapDrawContext,
+  emit: LbsMapEmitter
+) {
+  // 初始化设置基础瓦片层坐标系
+  _dc.baseTileCoords = getBaseTileCoords(props);
+  //--------------------------------------------
+  // 帮助方法
+  //--------------------------------------------
+  function Icon(
+    urlOrIcon: IconInput,
+    options: LBSMapEditMarkerIconOptions = {}
+  ) {
+    if (!urlOrIcon) return new L.Icon.Default();
 
-        let {
-            size = 32,
-            className,
-            color = "primary",
-            iconSize = [24, 41],
-            iconAnchor = [12, 41],
-            shadow = true,
-            shadowSize = [41, 41],
-            shadowAnchor = [12, 41]
-        } = options;
+    let {
+      size = 32,
+      className,
+      color = "primary",
+      iconSize = [24, 41],
+      iconAnchor = [12, 41],
+      shadow = true,
+      shadowSize = [41, 41],
+      shadowAnchor = [12, 41],
+    } = options;
 
-        // Eval the icon
-        let icon = Icons.parseIcon(urlOrIcon)
+    // Eval the icon
+    let icon = Icons.parseIcon(urlOrIcon);
 
-        // Font icon
-        if ("font" == icon.type) {
-            let html = Icons.fontIconHtml(icon);
-            let ansz = size / 2
-            return L.divIcon({
-                className: `ti-gsi-mark-icon 
+    // Font icon
+    if ("font" == icon.type) {
+      let html = Icons.fontIconHtml(icon);
+      let ansz = size / 2;
+      return L.divIcon({
+        className: `ti-gsi-mark-icon 
                         is-size-${size} 
                         is-color-${color}
-                        ${shadow ? 'has-shadow' : ''}`,
-                html,
-                iconSize: [size, size],
-                iconAnchor: [ansz, ansz]
-            })
+                        ${shadow ? "has-shadow" : ""}`,
+        html,
+        iconSize: [size, size],
+        iconAnchor: [ansz, ansz],
+      });
+    }
+
+    // Image Icon
+    if ("image" == icon.type && icon.src) {
+      let shadowUrl: string | undefined = undefined;
+      if (shadow) {
+        if (_.isBoolean(shadow)) {
+          let [_, nmPath, suffix] = /^([^.]+)\.(\w+)$/.exec(icon.src) || [];
+          shadowUrl = `${nmPath}-shadow.${suffix}`;
+        } else {
+          shadowUrl = shadow;
         }
-
-        // Image Icon
-        if ("image" == icon.type && icon.src) {
-            let shadowUrl: string | undefined = undefined;
-            if (shadow) {
-                if (_.isBoolean(shadow)) {
-                    let [_, nmPath, suffix] = /^([^.]+)\.(\w+)$/.exec(icon.src) || [];
-                    shadowUrl = `${nmPath}-shadow.${suffix}`
-                } else {
-                    shadowUrl = shadow;
-                }
-                shadowUrl = GetIconSrc(shadowUrl)
-            }
-            return L.icon({
-                iconUrl: GetIconSrc(icon.src),
-                className,
-                iconSize, iconAnchor,
-                shadowUrl, shadowSize, shadowAnchor
-            })
-        }
-
-        // Keep original input
-        throw `Invalid icon type: ${urlOrIcon}`
+        shadowUrl = GetIconSrc(shadowUrl);
+      }
+      return L.icon({
+        iconUrl: GetIconSrc(icon.src),
+        className,
+        iconSize,
+        iconAnchor,
+        shadowUrl,
+        shadowSize,
+        shadowAnchor,
+      });
     }
 
-    //--------------------------------------
-    function GetIconSrc(src: string) {
-        if (/^(https?:\/\/|\/)/.test(src)) {
-            return src
-        }
-        return `${props.imageIconBase}${src}`
-    }
+    // Keep original input
+    throw `Invalid icon type: ${urlOrIcon}`;
+  }
 
-    //--------------------------------------
-    function updateGeoInfo() {
-
+  //--------------------------------------
+  function GetIconSrc(src: string) {
+    if (/^(https?:\/\/|\/)/.test(src)) {
+      return src;
     }
+    return `${props.imageIconBase}${src}`;
+  }
 
-    //--------------------------------------
-    function GeoStr(v?: number) {
-        if (_.isUndefined(v))
-            return ""
+  //--------------------------------------
+  function updateGeoInfo() {}
 
-        let p = props.latlngPrecise;
-        let s = '' + Num.precise(v, p);
-        let ss = s.split('.')
-        ss[1] = _.padEnd(ss[1], p, '0')
-        return ss.join('.')
+  //--------------------------------------
+  function GeoStr(v?: number) {
+    if (_.isUndefined(v)) return "";
+
+    let p = props.latlngPrecise;
+    let s = "" + Num.precise(v, p);
+    let ss = s.split(".");
+    ss[1] = _.padEnd(ss[1], p, "0");
+    return ss.join(".");
+  }
+  //--------------------------------------
+  function GeoPointStr(obj: LatLngObj) {
+    let ss = [GeoStr(obj.lat), GeoStr(obj.lng)];
+    return ss.join(", ");
+  }
+  //--------------------------------------
+  // 数据转换方法
+  //--------------------------------------
+  const _val_coords = props.valueCoords ?? "WGS84";
+  //--------------------------------------
+  function trans_obj_from_tiles_to_value(obj: LatLngObj) {
+    return translateCoordsForLatlngObj(_dc.baseTileCoords, _val_coords, obj);
+  }
+  function trans_obj_from_value_to_tiles(obj: LatLngObj) {
+    return translateCoordsForLatlngObj(_val_coords, _dc.baseTileCoords, obj);
+  }
+  //--------------------------------------
+
+  //--------------------------------------
+  // 响应事件
+  //--------------------------------------
+  function OnMapMove(evt: L.LeafletEvent) {
+    let { $map } = _dc;
+    if (!$map) {
+      return;
     }
-    //--------------------------------------
-    function GeoPointStr(obj: LatLngObj) {
-        let ss = [
-            GeoStr(obj.lat),
-            GeoStr(obj.lng)
-        ]
-        return ss.join(", ");
+    //console.log("map move", evt)
+    let now = Date.now();
+    let bou = $map.getBounds();
+    _dc.geo = {
+      zoom: $map.getZoom(),
+      center: bou.getCenter(),
+      SW: bou.getSouthWest(),
+      SE: bou.getSouthEast(),
+      NE: bou.getNorthEast(),
+      NW: bou.getNorthWest(),
+      W: bou.getWest(),
+      E: bou.getEast(),
+      S: bou.getSouth(),
+      N: bou.getNorth(),
+    };
+    // Keep zoom in local
+    if (props.keepZoomBy) {
+      Ti.Storage.local.set(this.keepZoomBy, this.geo.zoom);
     }
-    //--------------------------------------
+    // If cooling, notify
+    if (!this.__check_cooling && this.cooling > 0) {
+      this.__check_cooling = true;
+      window.setTimeout(() => {
+        this.checkMoveCooling();
+      }, this.cooling + 10);
+    }
+    // lastMove for cooling
+    this.lastMove = now;
+  }
+  //--------------------------------------
+  function OnMapPointerClick(evt: L.LeafletMouseEvent) {}
+  //--------------------------------------
+  function OnMapPointerMove(evt: L.LeafletMouseEvent) {}
+
+  //--------------------------------------
+  // 通知改动
+  //--------------------------------------
+  function notifyChange(change: LBSMapData) {
+    if (!_.isEqual(change, props.value)) {
+      emit("change", change);
+    }
+  }
+
+  //--------------------------------------
+  // 返回特性
+  //--------------------------------------
+  return {
+    Icon,
+    GetIconSrc,
+    updateGeoInfo,
+    GeoStr,
+    GeoPointStr,
     // 数据转换方法
-    //--------------------------------------
-    const _val_coords = props.valueCoords ?? 'WGS84'
-    //--------------------------------------
-    function trans_obj_from_tiles_to_value(obj: LatLngObj) {
-        return translateCoordsForLatlngObj(_dc.baseTileCoords, _val_coords, obj);
-    }
-    function trans_obj_from_value_to_tiles(obj: LatLngObj) {
-        return translateCoordsForLatlngObj(_val_coords, _dc.baseTileCoords, obj);
-    }
-    //--------------------------------------
-
-    //--------------------------------------
-    // 初始化地图组件
-    //--------------------------------------
-
-    //--------------------------------------
+    trans_obj_from_tiles_to_value,
+    trans_obj_from_value_to_tiles,
+    // 响应事件
+    OnMapMove,
+    OnMapPointerClick,
+    OnMapPointerMove,
     // 通知改动
-    //--------------------------------------
-    function notifyChange(change: LBSMapData) {
-        if (!_.isEqual(change, props.value)) {
-            emit('change', change);
-        }
-    }
-
-    //--------------------------------------
-    // 返回特性
-    //--------------------------------------
-    return {
-        Icon, GetIconSrc, updateGeoInfo,
-        GeoStr, GeoPointStr,
-        // 数据转换方法
-        trans_obj_from_tiles_to_value,
-        trans_obj_from_value_to_tiles,
-        // 通知改动
-        notifyChange
-    }
+    notifyChange,
+  };
 }
 
 export function getBaseTileCoords(props: LbsMapProps): LBSMapValueCoords {
-    if (_.isEmpty(props.tileLayer)) {
-        return 'WGS84';
-    }
-    let tl = _.first(_.concat(props.tileLayer ?? []))
-    if (!tl) {
-        return 'WGS84';
-    }
-    let tile: LBSMapTileLayer;
-    if (isLBSMapStdTileType(tl)) {
-        tile = getLBSMapStdTileLayer(tl);
-    } else {
-        tile = tl;
-    }
-    return tile.coords ?? 'WGS84'
+  if (_.isEmpty(props.tileLayer)) {
+    return "WGS84";
+  }
+  let tl = _.first(_.concat(props.tileLayer ?? []));
+  if (!tl) {
+    return "WGS84";
+  }
+  let tile: LBSMapTileLayer;
+  if (isLBSMapStdTileType(tl)) {
+    tile = getLBSMapStdTileLayer(tl);
+  } else {
+    tile = tl;
+  }
+  return tile.coords ?? "WGS84";
 }
