d080529b235c879763853fc7703928c379fabec0
Peter Zhang <zozohtnt@gmail.com>
2025-06-18 15:57:50 +0800

    refactor(rich-prose): 使用异步组件加载富文本编辑器

--- src/lib/edit/rich/prose/ti-edit-rich-prose-index.ts
+++ src/lib/edit/rich/prose/ti-edit-rich-prose-index.ts
@@ -1,22 +1,30 @@
 import { TiComInfo, TiComRace } from "@site0/tijs";
-import { App } from "vue";
+import { App, defineAsyncComponent } from "vue";
 import { COM_TYPES } from "../../../lib-com-types";
-import TiEditRichProse from "./TiEditRichProse.vue";
+//import TiEditRichProse from "./TiEditRichProse.vue";
+const COM_TYPE = COM_TYPES.EditRichProse;
+const TiEditRichProse = defineAsyncComponent({
+  loader: () => import("./TiEditRichProse.vue"),
+  loadingComponent: {
+    template: `<div>Loading ${COM_TYPE}.vue...</div>`,
+  },
+  errorComponent: {
+    template: `<div>Fail to async load ${COM_TYPE}.vue</div>`,
+  },
+});
 
 const en_us = {
-  'com-name': 'Rich Prose Editor',
+  "com-name": "Rich Prose Editor",
 };
 const zh_cn = {
-  'com-name': '富文本编辑器',
+  "com-name": "富文本编辑器",
 };
-
-const COM_TYPE = COM_TYPES.EditRichProse;
 
 const TiEditRichProseInfo: TiComInfo = {
   icon: "zmdi-edit",
   race: TiComRace.EDIT,
   name: COM_TYPE,
-  text: 'i18n:ti-edit-rich-prose-com-name',
+  text: "i18n:ti-edit-rich-prose-com-name",
   i18n: {
     en_us: en_us,
     en_uk: en_us,
