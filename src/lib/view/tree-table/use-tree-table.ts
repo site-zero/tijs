640bd286aa6ef49c3dde95ba1f1f76bffb3cc7a0
Peter Zhang <zozohtnt@gmail.com>
2025-09-18 11:41:31 +0800

    ...

--- src/lib/view/tree-table/use-tree-table.ts
+++ src/lib/view/tree-table/use-tree-table.ts
@@ -1,7 +1,6 @@
 import _ from 'lodash';
 import { AnyOptionItem, TableRowID, Vars } from '../../../_type';
 import { EventUtils, I18n, Tmpl, Util } from '../../../core';
-import { getLogger } from '../../../core/log/ti-log';
 import {
   SelectableState,
   useDataLogicType,
@@ -13,8 +12,6 @@ import {
   TreeTableProps
 } from './ti-tree-table-types';
 import { TreeEmitter } from '../tree/ti-tree-types';
-
-const log = getLogger('TiList.use-list');
 
 export function useList(_props: TreeTableProps, _emit: TreeEmitter) {
 
