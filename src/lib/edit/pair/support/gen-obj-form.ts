f9a6515b774165dbbaff6f3fb27e2c7c696acc67
Peter Zhang <zozohtnt@gmail.com>
2026-04-25 19:29:12 +0800

    ...

--- src/lib/edit/pair/support/gen-obj-form.ts
+++ src/lib/edit/pair/support/gen-obj-form.ts
@@ -1,9 +1,45 @@
-import { GridFieldsInput, Vars } from "@site0/tijs";
+import { FieldRefer, FieldValueType, getFieldTypeByValue, GridFieldsInput, InputBoxProps, InputNumProps, ToggleProps, Vars } from "@site0/tijs";
 import _ from "lodash";
 
-export function gen_obj_form_fields(obj: Vars): Partial<GridFieldsInput>[] {
+const DEFAULT_FIELDS: Record<FieldValueType, FieldRefer> = {
+  "Boolean": {
+    type:"Boolean",
+    comType:"TiToggle",
+    comConf: {} as ToggleProps
+  },
+  "Number": {
+    type:"Number",
+    comType:"TiInputNum",
+    comConf: {} as InputNumProps
+  },
+  "Float": {
+    type:"Number",
+    comType:"TiInputNum",
+    comConf: {
+
+    } as InputNumProps
+  },
+  "String": {
+    type:"String",
+    comType:"TiInput",
+    comConf: {} as InputBoxProps
+  },
+  "Array": {
+    type:"Array",
+    comType:"TiInputArray",
+    comConf: {} as InputArrayProps
+  },
+};
+
+export function gen_obj_form_fields(obj: Vars, defaultFields?: Record<FieldValueType, FieldRefer>): Partial<GridFieldsInput>[] {
   let re = [] as Partial<GridFieldsInput>[];
   for (let key of _.keys(obj)) {
+    let val = obj[key];
+    let vtp = getFieldTypeByValue(val);
+    re.push({
+      field: key,
+      value: val,
+    });
   }
   return re;
 }
